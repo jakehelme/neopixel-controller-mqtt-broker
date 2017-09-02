bcbbc9396316ff293e2dcb49add130ee1a2c1dba
Jake Helme <jake.helme@gmail.com>
2017-09-02 21:10:20 +0200

    Added prompt for putting in mongodb details

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
+const readlineSync = require('readline-sync');
 let mosca = require('mosca');
 
-let ascoltatore = {
-	//using ascoltatore
-	type: 'mongo',
-	url: process.env.MQTT_BROKER_MONGODB,
-	pubsubCollection: process.env.MQTT_DB,
-	mongo: {}
-};
-
-let settings = {
-	port: 1883,
-	backend: ascoltatore
-};
-
-let server = new mosca.Server(settings);
-
-server.on('clientConnected', function (client) {
-	console.log('client connected', client.id);
-});
-
-// fired when a message is received
-server.on('published', function (packet) {
-	console.log('Published', packet.payload);
-});
-
-server.on('ready', setup);
-
-// fired when the mqtt server is ready
-function setup() {
-	console.log('Mosca server is up and running');
+if (!process.env.MQTT_BROKER_MONGODB && !process.env.MQTT_DB) {
+	const connectionString = readlineSync.question('Connection string: ');
+	const collection = readlineSync.question('Collection: ');
+	process.env.MQTT_BROKER_MONGODB = connectionString;
+	process.env.MQTT_DB = collection;
 }
+
+function startBroker() {
+	let backend = {
+		//using ascoltatore
+		type: 'mongo',
+		url: process.env.MQTT_BROKER_MONGODB,
+		pubsubCollection: process.env.MQTT_DB,
+		mongo: {}
+	};
+
+	let settings = {
+		port: 1883,
+		backend
+	};
+
+	let server = new mosca.Server(settings);
+
+	server.on('clientConnected', function (client) {
+		console.log('client connected', client.id);
+	});
+
+	// fired when a message is received
+	server.on('published', function (packet) {
+		console.log('Published', packet.payload);
+	});
+
+	server.on('ready', () => { console.log('Mosca server is up and running'); });
+}
+
+startBroker();
